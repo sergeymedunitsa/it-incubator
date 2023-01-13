1fb1fd78dbeb9077c84b5932eb591baf64a24168
Sergry Medunitsa <sergeymedunitsa@gmail.com>
2023-01-14 01:27:27 +0300

    Beginner Series #2 Clock

--- codewars/BeginnerSeries#2Clock.js
+++ codewars/BeginnerSeries#2Clock.js
@@ -15,6 +15,6 @@
 // 0 <= s <= 59
 
 function past(h, m, s){
-    return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000)
+    return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
     //#Happy Coding! ^_^
 }
