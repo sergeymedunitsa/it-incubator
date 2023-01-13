0191d29474b4449c762a5c31472a1dfdbea43e1d
Sergry Medunitsa <sergeymedunitsa@gmail.com>
2023-01-13 19:39:40 +0300

    Use Bracket Notation to Find the Nth-to-Last Character in a String

---
+++ freecodecamp/14.js
@@ -0,0 +1,19 @@
+// Используйте скобки, чтобы найти энный по счету символ в строке
+// Вы можете использовать тот же принцип, который мы только что 
+// использовали для извлечения последнего символа в строке, 
+// чтобы получить от n-го до последнего символа.
+// Например, вы можете получить значение предпоследней буквы 
+// const firstName = "Augusta"строки, 
+// используя firstName[firstName.length - 3]
+// Пример:
+// const firstName = "Augusta";
+// const thirdToLastLetter = firstName[firstName.length - 3];
+// thirdToLastLetter будет иметь значение строки s.
+// Используйте скобки, чтобы найти предпоследний символ в строке lastName.
+// Подсказка: Попробуйте посмотреть на приведенный выше пример, если вы застряли.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
