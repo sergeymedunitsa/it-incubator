3d2248739b078c2d26c31aadb420e48677295463
Sergry Medunitsa <sergeymedunitsa@gmail.com>
2023-01-13 18:06:34 +0300

    Constructing Strings with Variables

---
+++ freecodecamp/10.js
@@ -0,0 +1,6 @@
+// Sometimes you will need to build a string. By using the concatenation operator (+), 
+// you can insert one or more variables into a string you're building.
+
+// Only change code below this line
+const myName = "Sergey";
+const myStr = "My name is " + myName + " and I am well!";
