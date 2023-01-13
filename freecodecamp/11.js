c6992d96624e5613b90fd0f033c29055d3b4559b
Sergry Medunitsa <sergeymedunitsa@gmail.com>
2023-01-13 18:16:30 +0300

    Appending Variables to Strings

---
+++ freecodecamp/11.js
@@ -0,0 +1,9 @@
+// Appending Variables to Strings
+
+// Change code below this line
+const someAdjective = "Free Code Camp";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Just as we can build a string over multiple lines out of string literals, 
+// we can also append variables to a string using the plus equals (+=) .
